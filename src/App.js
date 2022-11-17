5809ba48bbbafec2b2958e6a81d1635f7a09551e
Hansen Limanta <hansenlimanta@gmail.com>
2022-11-17 17:14:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// Components and pages
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+// Styles
+import GlobalStyles from "./components/GlobalStyles";
+// Router
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:id" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
